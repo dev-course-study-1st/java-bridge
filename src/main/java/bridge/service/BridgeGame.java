23d16dd30e363c7f922cf338611fe6b70231203a
NaMinhyeok <nmh9097@gmail.com>
2024-08-22 16:02:49 +0900

    feat: 재시작 기능 구현

--- src/main/java/bridge/service/BridgeGame.java
+++ src/main/java/bridge/service/BridgeGame.java
@@ -33,7 +33,17 @@ public class BridgeGame {
      * <p>
      * 재시작을 위해 필요한 메서드의 반환 타입(return type), 인자(parameter)는 자유롭게 추가하거나 변경할 수 있다.
      */
-    public void retry() {
+    public boolean retry(String gameCommand) {
+        if(gameCommand.equals(RestartCommand.RESTART.getCommand())) {
+            userBridge.clear();
+            tryCount++;
+            return true;
+        }
+        return false;
+    }
+
+    public boolean isGameEnd() {
+        return bridge.getBridge().size() == userBridge.size();
     }
 
     public List<String> getUserBridge() {
