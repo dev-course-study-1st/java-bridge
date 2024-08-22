7dacf79022e934b08485a9f6885c5694af1ce9c3
NaMinhyeok <nmh9097@gmail.com>
2024-08-22 15:59:16 +0900

    feat: 입출력 기능 구현

--- src/main/java/bridge/view/OutputView.java
+++ src/main/java/bridge/view/OutputView.java
@@ -1,5 +1,8 @@
 package bridge.view;
 
+import java.util.List;
+import java.util.StringJoiner;
+
 /**
  * 사용자에게 게임 진행 상황과 결과를 출력하는 역할을 한다.
  */
@@ -10,7 +13,41 @@ public class OutputView {
      * <p>
      * 출력을 위해 필요한 메서드의 인자(parameter)는 자유롭게 추가하거나 변경할 수 있다.
      */
-    public void printMap() {
+    public void printMap(List<String> userBridge, List<String> gameBridge) {
+        System.out.println(createBridge(userBridge,gameBridge));
+    }
+
+    private String createBridge(List<String> userBridge, List<String> gameBridge) {
+        // 여기서 userBridge 와 gameBridge를 비교한다.
+        // index가 같을 때 같은 문자열이면 O를 출력 아니면 X를 출력
+        StringJoiner bridge = new StringJoiner("\n");
+
+        StringJoiner upBridge = new StringJoiner(" | ");
+        StringJoiner downBridge = new StringJoiner(" | ");
+
+        for(int i=0; i<userBridge.size(); i++) {
+            if (userBridge.get(i).equals(gameBridge.get(i))) {
+                if (userBridge.get(i).equals("U")) {
+                    upBridge.add("O");
+                    downBridge.add(" ");
+                }
+                if (userBridge.get(i).equals("D")) {
+                    upBridge.add(" ");
+                    downBridge.add("O");
+                }
+            }
+            if (!userBridge.get(i).equals(gameBridge.get(i))) {
+                if (userBridge.get(i).equals("U")) {
+                    upBridge.add("X");
+                    downBridge.add(" ");
+                }
+                if (userBridge.get(i).equals("D")) {
+                    upBridge.add(" ");
+                    downBridge.add("X");
+                }
+            }
+        }
+        return bridge.add(String.format("[ %s ]", upBridge)).add(String.format("[ %s ]", downBridge)).toString();
     }
 
     /**
@@ -18,6 +55,13 @@ public class OutputView {
      * <p>
      * 출력을 위해 필요한 메서드의 인자(parameter)는 자유롭게 추가하거나 변경할 수 있다.
      */
-    public void printResult() {
+    public void printResult(boolean isGameEnd, int tryCount) {
+        System.out.println("최종 게임 결과");
+        System.out.println("게임 성공 여부: " + (isGameEnd ? "성공" : "실패"));
+        System.out.println("총 시도한 횟수: " + tryCount);
+    }
+
+    public void initGame() {
+        System.out.println("다리 건너기 게임을 시작합니다.");
     }
 }
