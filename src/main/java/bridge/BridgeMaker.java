2a96f70dd9334db5e3261edb4f72fbd230a32409
NaMinhyeok <nmh9097@gmail.com>
2024-08-22 16:00:04 +0900

    feat: 다리 생성기 구현

--- src/main/java/bridge/BridgeMaker.java
+++ src/main/java/bridge/BridgeMaker.java
@@ -1,5 +1,8 @@
 package bridge;
 
+import bridge.constant.BridgeGameConstant;
+
+import java.util.ArrayList;
 import java.util.List;
 
 /**
@@ -18,6 +21,32 @@ public class BridgeMaker {
      * @return 입력받은 길이에 해당하는 다리 모양. 위 칸이면 "U", 아래 칸이면 "D"로 표현해야 한다.
      */
     public List<String> makeBridge(int size) {
-        return null;
+        validate(size);
+        List<String> bridge = new ArrayList<>();
+        for(int i=0; i<size; i++) {
+            bridge.add(generateBridgeSegment());
+        }
+        return bridge;
+    }
+
+    private String generateBridgeSegment() {
+        int bridgeNumber = bridgeNumberGenerator.generate();
+        if(bridgeNumber==0) {
+            return "D";
+        }
+        if(bridgeNumber==1) {
+            return "U";
+        }
+        throw new IllegalArgumentException("다리의 형태는 0 또는 1이어야 합니다.");
+    }
+
+    private void validate(int size) {
+        if(!validateBridgeSize(size)) {
+            throw new IllegalArgumentException("다리의 길이는 " + BridgeGameConstant.BRIDGE_MIN_LENGTH.getValue() + " 이상 " + BridgeGameConstant.BRIDGE_MAX_LENGTH.getValue() + " 이하여야 합니다.");
+        }
+    }
+
+    private boolean validateBridgeSize(int size) {
+        return size >= BridgeGameConstant.BRIDGE_MIN_LENGTH.getValue() && size <= BridgeGameConstant.BRIDGE_MAX_LENGTH.getValue();
     }
 }
