7aa758bb536a2f9bd0773bc8e963c5fcbe23956e
junho <>
2024-08-31 11:46:54 +0900

    refactor: 메서드 분리

--- src/main/java/bridge/view/InputView.java
+++ src/main/java/bridge/view/InputView.java
@@ -5,8 +5,6 @@ import bridge.util.constant.Command;
 import bridge.util.constant.MoveKey;
 import camp.nextstep.edu.missionutils.Console;
 
-import static bridge.util.constant.Errors.INVALID_COMMAND_KEY_ERROR;
-import static bridge.util.constant.Errors.INVALID_MOVE_KEY_ERROR;
 import static bridge.util.constant.Messages.*;
 import static bridge.util.constant.Messages.INPUT_MOVE_KEY_MSG;
 
@@ -20,45 +18,50 @@ public class InputView {
      * 다리의 길이를 입력받는다.
      */
     public BridgeLength readBridgeSize() {
-        BridgeLength bridgeLength = null;
-        while (bridgeLength == null) {
-            System.out.println(INPUT_LENGTH_MSG.getMessage());
-            try {
-                bridgeLength = new BridgeLength(Console.readLine().trim());
-            } catch (IllegalArgumentException e) {
-                OutputView.printError(e);
-            }
+        try {
+            return requestBridgeLength();
+        } catch (IllegalArgumentException e) {
+            OutputView.printError(e);
+            return readBridgeSize();
         }
-        return bridgeLength;
+    }
+
+    private BridgeLength requestBridgeLength() {
+        System.out.println(INPUT_LENGTH_MSG.getMessage());
+        return new BridgeLength(Console.readLine().trim());
     }
 
     /**
      * 사용자가 이동할 칸을 입력받는다.
      */
     public MoveKey readMoving() {
-        while (true) {
-            try {
-                System.out.println(INPUT_MOVE_KEY_MSG.getMessage());
-                return MoveKey.getEnum(Console.readLine().trim())
-                        .orElseThrow(() -> new IllegalArgumentException(INVALID_MOVE_KEY_ERROR.getMessage()));
-            } catch (IllegalArgumentException e) {
-                OutputView.printError(e);
-            }
+        try {
+            return requestMoveKey();
+        } catch (IllegalArgumentException e) {
+            OutputView.printError(e);
+            return readMoving();
         }
+    }
+
+    private static MoveKey requestMoveKey() {
+        System.out.println(INPUT_MOVE_KEY_MSG.getMessage());
+        return MoveKey.getEnum(Console.readLine().trim());
     }
 
     /**
      * 사용자가 게임을 다시 시도할지 종료할지 여부를 입력받는다.
      */
     public Command readGameCommand() {
-        while (true) {
-            try {
-                System.out.println(INPUT_RETRY_OR_QUIT_MSG.getMessage());
-                return Command.getEnum(Console.readLine().trim())
-                        .orElseThrow(() -> new IllegalArgumentException(INVALID_COMMAND_KEY_ERROR.getMessage()));
-            } catch (IllegalArgumentException e) {
-                OutputView.printError(e);
-            }
+        try {
+            return requestRetryOrExit();
+        } catch (IllegalArgumentException e) {
+            OutputView.printError(e);
+            return readGameCommand();
         }
+    }
+
+    private static Command requestRetryOrExit() {
+        System.out.println(INPUT_RETRY_OR_QUIT_MSG.getMessage());
+        return Command.getEnum(Console.readLine().trim());
     }
 }
